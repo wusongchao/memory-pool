89d81735960d4f7dea3ae7f2c8de365a945c261b
yutianhuihong <2256912385@qq.com>
2018-11-05 20:43:15 +0800

    simplifiy the design

--- FixedMemoryPool.h
+++ FixedMemoryPool.h
@@ -27,62 +27,80 @@ using std::make_unique;
 
 // The class MemoryBlock should be completely managed by class MemoryPool
 // Similar to package private accessibility
-template
-<typename T>
-class FixedMemoryPool;
+// Just a private nested class that can ensure that only the FixedMemoryPool can access the MemoryPool
+// Don't need to complicate the problem and using method like: http://seanmiddleditch.com/enabling-make_unique-with-private-constructors/
+// like below
 
-template<typename T>
-class MemoryBlock
-{
-    friend class FixedMemoryPool<T>;
-    using blockPtr = unique_ptr<MemoryBlock<T>>;
-    struct _ConstructorTag { explicit _ConstructorTag() = default; };
-public:
-    MemoryBlock(size_t blockSize, _ConstructorTag)
-        :data(reinterpret_cast<T*>(operator new(sizeof(T) * blockSize))), allocatedCounter(0), next(nullptr)
-    {
-    }
-
-    ~MemoryBlock()
-    {
-        for (size_t i = 0; i != allocatedCounter; i++) {
-            (data + i) -> ~T();
-        }
-        operator delete(data);
-    }
-private:
-    T* data;
-    // raw pointer for these reasons:
-    // 1. The interface of MemoryPool return raw pointer
-    // 2. The semantic of MemoryPool's newElement is shared_ptr
-    // 3. Even if use the smart pointer, We have to define own delete startegy
-    
-    size_t allocatedCounter;
-
-    blockPtr next;
-
-    template
-    <typename... Args>
-    T* construct(Args&&... args)
-    {
-        cout << allocatedCounter << endl;
-        return new (data + (allocatedCounter++)) T(std::forward<Args>(args)...);
-    }
-
-    MemoryBlock(const MemoryBlock&) = delete;
-    MemoryBlock& operator=(const MemoryBlock&) = delete;
-
-};
+//template
+//<typename T>
+//class MemoryBlock
+//{
+//    friend class FixedMemoryPool<T>;
+//    struct _ConstructorTag { explicit _ConstructorTag() = default };
+//public:
+//    MemoryBlock(size_t blockSize, _ConstructorTag)
+//    {
+//        // ...
+//    }
+//};
+//template
+//<typename T>
+//class FixedMemoryPool
+//{
+//    FixedMemoryPool(size_t blockSize = 64)
+//        :blockSize(blockSize), head(make_unique<MemoryBlock<T>>(blockSize, MemoryBlock<T>::_ConstructorTag{}))
+//    {
+//    }
+//};
 
 template
 <typename T>
 class FixedMemoryPool 
 {
+    template<typename T>
+    struct MemoryBlock
+    {
+        friend class FixedMemoryPool<T>;
+        using blockPtr = unique_ptr<MemoryBlock<T>>;
+        //struct _ConstructorTag { explicit _ConstructorTag() = default; };
+        MemoryBlock(size_t blockSize)
+            :data(reinterpret_cast<T*>(operator new(sizeof(T) * blockSize))), allocatedCounter(0), next(nullptr)
+        {
+        }
+
+        ~MemoryBlock()
+        {
+            for (size_t i = 0; i != allocatedCounter; i++) {
+                (data + i) -> ~T();
+            }
+            operator delete(data);
+        }
+        T* data;
+        // raw pointer for these reasons:
+        // 1. The interface of MemoryPool return raw pointer
+        // 2. The semantic of MemoryPool's newElement is shared_ptr
+        // 3. Even if use the smart pointer, We have to define own delete startegy
+
+        size_t allocatedCounter;
+
+        blockPtr next;
+
+        template
+            <typename... Args>
+            T* construct(Args&&... args)
+        {
+            return new (data + (allocatedCounter++)) T(std::forward<Args>(args)...);
+        }
+
+        MemoryBlock(const MemoryBlock&) = delete;
+        MemoryBlock& operator=(const MemoryBlock&) = delete;
+    };
+
 public:
     using valueType = T;
 
     FixedMemoryPool(size_t blockSize = 64)
-        :blockSize(blockSize), head(make_unique<MemoryBlock<T>>(blockSize, MemoryBlock<T>::_ConstructorTag{}))
+        :blockSize(blockSize), head(make_unique<MemoryBlock<T>>(blockSize))
     {
     }
 
@@ -106,7 +124,7 @@ public:
 private:
     void expand()
     {
-        auto newBlock = make_unique<MemoryBlock<T>>(blockSize, MemoryBlock<T>::_ConstructorTag{});
+        auto newBlock = make_unique<MemoryBlock<T>>(blockSize);
         newBlock->next = std::move(head);
         head = std::move(newBlock);
     }
